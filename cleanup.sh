e717e733613600dff2cd97ed6a9929cb3172b51d
Tricky <rick@tricky.me.uk>
2021-10-14 10:59:26 +0100

    Update

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-docker stop ftpserver
-docker rm ftpserver
-
-sudo rm -r /home/tricky/docker/webserver
-
-mkdir /home/tricky/docker/webserver
-#sudo chown -R -f www-data:www-data /home/tricky/docker/webserver
